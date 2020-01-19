1316f9b0b235ea74ece7236228009195065699e0
snipernoble3 <natelow@rocketmail.com>
2020-01-19 15:25:01 -0800

    Weapon Manager Script
    Added a better pass through for the animation driven methods in the gun scripts

---
+++ GravityBlast/Assets/Scripts/Weapon Scripts/WeaponManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+
+    [SerializeField] GameObject[] GunList;
+    
+    [SerializeField] int startingGun;
+    int currGun;
+
+    private void Start () {
+        currGun = startingGun;
+    }
+
+    //reload passthrough
+    void AnimReload () {
+        GunList[currGun].GetComponent<IProjectileWeapon>().Reload();
+    }
+
+    //swap gun
+
+}
