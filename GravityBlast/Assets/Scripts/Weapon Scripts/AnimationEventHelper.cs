02651df83aadd7a58e4168477fb4e096e9fde159
Xury Greer <Xury.Greer@Gmail.com>
2020-01-25 20:33:48 -0800

    Reorganized some of the weapon scripts. Removed weapon prefab from the player prefab. Modified Weapon Manager script so that it instantiates all weapons on level load.

---
+++ GravityBlast/Assets/Scripts/Weapon Scripts/AnimationEventHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventHelper : MonoBehaviour
+{
+	[SerializeField] private WeaponManager weaponManager;
+	
+    // Update is called once per frame
+    void ReloadEvent()
+    {
+        weaponManager.Reload();
+    }
+}
--- GravityBlast/Assets/Scripts/Weapon Scripts/WeaponManager.cs
+++ GravityBlast/Assets/Scripts/Weapon Scripts/WeaponManager.cs
@@ -1,23 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WeaponManager : MonoBehaviour {
 
-    [SerializeField] GameObject[] GunList;
+	[SerializeField] private GameObject[] weaponPrefabs; // An array of all weapon prefabs the player can have.
+	private GameObject[] weapons; // An array of the instantiated weapons the player has.
+	[SerializeField] private Transform[] weaponBones; // The weapon "bone" positions in the armature to attach the weapons to.
+	[SerializeField] private Animator animator; // A reference to Gabriel's animation system.
+	[SerializeField] private TextMeshProUGUI ammoUI;
     
-    [SerializeField] int startingGun;
-    int currGun;
+    //[SerializeField] private int startingGun;
+    private int currentWeapon = 0;
 
-    private void Start () {
-        currGun = startingGun;
+    private void Awake()
+	{
+		
+		weapons = new GameObject[weaponPrefabs.Length];
+		
+		for (int i = 0; i < weapons.Length; i++)
+		{
+			weapons[i] = Instantiate(weaponPrefabs[i]); // Instantiate the weapon.
+			weapons[i].transform.parent = weaponBones[1]; // Make the weapon a child of the right hand weapon bone.
+			weapons[i].transform.localPosition = Vector3.zero; // Clear out the transform so it aligns with the weapon bone.
+			//weapons[i].transform.localRotation = Quaternion.identity;
+			weapons[i].transform.localRotation = Quaternion.Euler(Vector3.right * 90.0f);
+			weapons[i].transform.localScale = Vector3.one;
+			
+			// Set up references.
+			weapons[i].GetComponent<Deliverance>().player = gameObject;
+			weapons[i].GetComponent<Deliverance>().arms = animator;
+			weapons[i].GetComponent<Deliverance>().ammoUI = ammoUI;
+			
+			weapons[i].SetActive(false); // Hide the weapon.
+        }
+		
+		//currentWeapon = startingGun;
+		//SwitchWeapon(startingGun);
+		SwitchWeapon(currentWeapon);
     }
 
     //reload passthrough
-    void AnimReload () {
-        GunList[currGun].GetComponent<IProjectileWeapon>().Reload();
+    public void Reload()
+	{
+        weapons[currentWeapon].GetComponent<IProjectileWeapon>().Reload();
+    }
+	
+	//test multiple weapons
+    void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.Alpha0)) SwitchWeapon(0);
+		if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchWeapon(0);
+		if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchWeapon(1);
+		if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchWeapon(2);
     }
 
-    //swap gun
-
+    //switch weapon
+	void SwitchWeapon(int weaponOfChoice)
+	{
+		if (weaponOfChoice >= 0 && weaponOfChoice < weapons.Length)
+		{		
+			foreach (GameObject weapon in weapons)
+			{
+				weapon.SetActive(false);
+			}
+			weapons[weaponOfChoice].SetActive(true);
+		}
+	}
 }
