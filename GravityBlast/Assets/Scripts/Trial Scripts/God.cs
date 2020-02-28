3b3ac26d2e8593b379cdbacfec33891c7c624cd5
snipernoble3 <natelow@rocketmail.com>
2020-02-27 21:54:03 -0800

    Planet Spawning Start
    Pseudocode for planet generation

---
+++ GravityBlast/Assets/Scripts/Trial Scripts/God.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class God : MonoBehaviour {
+    
+    //level tracker
+    //player
+    //current solar system
+    //planet prefabs
+
+    private void GenerateSolarSystem () {
+        
+    }
+
+    public void NextPlanet () {
+        //destroy old planet
+
+        //create new planet
+        //pick size in range
+        //assign size class
+    }
+
+    private void SizeClassOf (float size) {
+
+        //determine the class by how it compares to the size cutoffs for small med large
+        switch (size) {
+            case 0:
+                break;
+                
+        }
+    }
+
+}
---
+++ GravityBlast/Assets/Scripts/Trial Scripts/PlanetManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour {
+
+    // PLANET INFO //
+    //type?
+    //difficulty/number of stage
+    //size category
+    //boss
+    //moon
+
+    // ENEMIES //
+    //enemies to spawn
+    //enemy pool
+
+    public void LoadPlanet () { //called by game manager
+        //spawn environment
+        //spawn enemies set as inactive
+        //place enemies based on planet details
+        //spawn moon if required
+    }
+
+    public void DestroyPlanet () {
+        //destruction animation?
+        //remove enemies
+    }
+
+    private void PopulateEnvironment () {
+
+    }
+
+    private void PopulateEnemies () {
+
+    }
+
+    private void GenerateMoon () {
+
+    }
+
+}
