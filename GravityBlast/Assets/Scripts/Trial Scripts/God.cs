9d3287a02cb8308d822cb4b818ddec518ddb04ba
snipernoble3 <natelow@rocketmail.com>
2020-03-19 18:55:16 -0700

    Enemies Spawn on Planets
    Currently just random spawning, no clustered spawning in place. Environment assets also do not spawn yet.

--- GravityBlast/Assets/Scripts/Trial Scripts/God.cs
+++ GravityBlast/Assets/Scripts/Trial Scripts/God.cs
@@ -14,11 +14,19 @@ public class God : MonoBehaviour {
         //size category
         public float size;
         public string sizeCategory;
+        public GameObject[] enemyPrefabs;
         //moon
         public bool hasMoon;
         //boss
         public bool hasBoss;
     }
+
+    /*
+    public struct EnemyInfo {
+        public GameObject prefab;
+        //float size; - if scale gets changed on different planets
+        public int maxQuantity;
+    }*/
 
     //level tracker
     int completedPlanets = 0;
@@ -31,9 +39,20 @@ public class God : MonoBehaviour {
     GameObject currPlanet;
     //planet prefabs
     [SerializeField] private GameObject[] planetPrefabs;
+    //enemy info
+    [SerializeField] private GameObject[] enemyPrefabs;
+    //[SerializeField] private int[] enemyMaxQuantities;
+    //private EnemyInfo[] enemies;
 
     float minPlanetScale = 0.5f;
     float maxPlanetScale = 1.0f;
+
+    private void Awake () {
+        //GenerateEnemyInfo();
+
+        GenerateSolarSystem();
+
+    }
 
     private void Update () {
         if (Input.GetKeyDown(KeyCode.L)) {
@@ -41,34 +60,79 @@ public class God : MonoBehaviour {
         }
     }
 
+    /*
+    void GenerateEnemyInfo () {
+        enemies = new EnemyInfo[enemyPrefabs.Length];
+        for (int i = 0; i < enemyPrefabs.Length; i++) {
+            enemies[i] = new EnemyInfo();
+            enemies[i].prefab = enemyPrefabs[i];
+            if (i < enemyMaxQuantities.Length) {
+                enemies[i].maxQuantity = enemyMaxQuantities[i];
+            } else {
+                enemies[i].maxQuantity = 50;
+            }
+        }
+
+
+    }*/
 
     private void GenerateSolarSystem () {
         //generate x planets (info)
+        int numPlanets = 3;
+        currSolarSystem = new PlanetInfo[numPlanets];
+        for (int i = 0; i < numPlanets; i++) {
+            currSolarSystem[i] = GeneratePlanetInfo(completedPlanets + i);
+        }
     }
 
-    private PlanetInfo GeneratePlanetInfo () {
+    private PlanetInfo GeneratePlanetInfo (int planetNumber) {
         PlanetInfo planet = new PlanetInfo();
         planet.prefab = planetPrefabs[Random.Range(0, planetPrefabs.Length)];
         planet.difficultySetting = 1;
-        planet.stageNumber = completedPlanets + 1;
+        planet.stageNumber = planetNumber;
         planet.size = Random.Range(minPlanetScale, maxPlanetScale);
         planet.sizeCategory = SizeClassOf(planet.size);
+        planet.enemyPrefabs = enemyPrefabs;
         planet.hasMoon = false;
         planet.hasBoss = false;
         return planet;
     }
 
-    public void NextPlanet () {
-        //destroy old planet
-        if (currPlanet != null) {
-            Destroy(currPlanet.gameObject);
-        }
-        //create new planet
-        PlanetInfo planetToCreate = GeneratePlanetInfo();
+    void FirstPlanet () {
+        PlanetInfo planetToCreate = currSolarSystem[planetInSolarSystem];
         currPlanet = Instantiate(planetToCreate.prefab, Vector3.zero, Quaternion.identity);
         currPlanet.GetComponent<PlanetManager>().SetInfo(planetToCreate);
         currPlanet.GetComponent<PlanetManager>().LoadPlanet();
         player.GetComponent<Gravity_AttractedObject>().SetGravitySource(currPlanet.GetComponent<Gravity_Source>());
+    }
+
+    public void NextPlanet () {
+
+        //step forward in planet progression
+        completedPlanets++;
+        planetInSolarSystem++;
+
+        //check to see if that was the end of the solar system
+        if (planetInSolarSystem == currSolarSystem.Length) {
+            GenerateSolarSystem();
+            planetInSolarSystem = 0;
+        }
+        
+        //destroy old planet
+        if (currPlanet != null) {
+            currPlanet.GetComponent<PlanetManager>().DestroyPlanet();
+        }
+
+        //create new planet
+        PlanetInfo planetToCreate = currSolarSystem[planetInSolarSystem];
+        currPlanet = Instantiate(planetToCreate.prefab, Vector3.zero, Quaternion.identity);
+        currPlanet.GetComponent<PlanetManager>().SetInfo(planetToCreate);
+        currPlanet.GetComponent<PlanetManager>().LoadPlanet();
+        player.GetComponent<Gravity_AttractedObject>().SetGravitySource(currPlanet.GetComponent<Gravity_Source>());
+        
+        //place player on planet
+
+
     }
 
     private string SizeClassOf (float size) {
--- GravityBlast/Assets/Scripts/Trial Scripts/PlanetManager.cs
+++ GravityBlast/Assets/Scripts/Trial Scripts/PlanetManager.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour {
 
-    struct EnemyInfo {
-        GameObject prefab;
-        float size;
-        int maxQuantity;
-    }
+
 
     // PLANET INFO //
     God.PlanetInfo planet;
     [SerializeField] GameObject killBounds;
 
     // ENEMIES //
-    //enemies to spawn
-    EnemyInfo[] enemyInfos;
     //enemy pool
-    GameObject[][] enemyPool;
+    GameObject[,] enemyPool;
 
-    public void LoadPlanet () { //called by game manager
+    public IEnumerator LoadPlanet () { //called by game manager
+        //set name
+        name = "Planet " + planet.stageNumber;
         //set size
         transform.localScale = planet.size * Vector3.one;
         //create kill boundaries
@@ -28,11 +24,14 @@ public class PlanetManager : MonoBehaviour {
         //scale kill boundaries - 70 scale
         k.transform.localScale = 70f * Vector3.one;
         //spawn environment
+        yield return new WaitForSeconds(0.1f);
         PopulateEnvironment();
         //spawn enemies
+        yield return new WaitForSeconds(0.1f);
         PopulateEnemies();
         //spawn moon if required
-
+        yield return new WaitForSeconds(0.1f);
+        if (planet.hasMoon) GenerateMoon();
         //add boss if required
 
 
@@ -40,27 +39,61 @@ public class PlanetManager : MonoBehaviour {
 
     public void DestroyPlanet () {
         //destruction animation?
-        //remove enemies
+
+        Destroy(this.gameObject);
     }
 
-    private void PopulateEnvironment () {
+    private IEnumerator PopulateEnvironment () {
         //spawn ground textures
 
         //spawn large constructs
+        yield return new WaitForSeconds(0.01f);
 
         //spawn plant clusters / small constructs
+        //yield return new WaitForSeconds(0.01f);
 
         //spawn random plants
+        yield return new WaitForSeconds(0.01f);
+
+        //spawn loose environment pieces
+        //yield return new WaitForSeconds(0.01f);
 
     }
 
     private void PopulateEnemies () {
+        enemyPool = new GameObject[planet.enemyPrefabs.Length, 50];
+        //for each enemy
+        for (int i = 0; i < planet.enemyPrefabs.Length; i++) {
+            for (int j = 0; j < 50; j++) { //spawn (<) x enemies
+                enemyPool[i, j] = Instantiate(planet.enemyPrefabs[i], RandomSpawnPoint(), Quaternion.identity);
+                enemyPool[i, j].transform.parent = transform;
+                j++;
+            }
+        }
 
-        //for each enemy :        
-        //determine how many enemies to spawn
+    }
 
-        //spawn them randomly around the surface
-
+    Vector3 RandomSpawnPoint () {
+        Vector3 spawnPoint;
+        //get random point on sphere larger than the surface
+        Vector3 outer = Random.onUnitSphere * (planet.size * 80f);
+        spawnPoint = outer;
+        //direction to planet
+        Vector3 dir = (outer - transform.position).normalized * -9.8f;
+        //raycast to point on planet
+        RaycastHit[] hits;
+        //Physics.Raycast(outer, dir, out hit, 100f);
+        //Debug.DrawLine(outer, hit.point, Color.green, 2f);
+        //Vector3 spawnPoint = hit.point;
+        hits = Physics.RaycastAll(outer, dir, Vector3.Distance(outer, transform.position));
+        for (int i = 0; i < hits.Length; i++) {
+            if (hits[i].transform == transform) {
+                spawnPoint = hits[i].point - (dir.normalized * 0.5f);
+                //Debug.DrawLine(outer, hits[i].point, Color.green, 2f);
+            }
+        }
+        //return value
+        return spawnPoint;
     }
 
     private void GenerateMoon () {
