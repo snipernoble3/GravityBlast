b37f96b93b0f251a68c98e1480c09ad2c6bd6223
Xury Greer <Xury.Greer@Gmail.com>
2020-04-04 12:13:06 -0700

    Replaced placeholder planet with a functioning planet positioning display in the HUD.

---
+++ GravityBlast/Assets/Scripts/PlanetHUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetHUD : MonoBehaviour
+{
+    [SerializeField] private GameObject planetHUDPrefab;
+	private GameObject planetWireframe;
+	private Player_Movement playerMovement;
+	
+	private Transform cameraPivot;
+	private Transform camera;
+	
+	private float cameraDistance = 1.2f;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        planetWireframe = Instantiate(planetHUDPrefab, Gravity_Source.DefaultGravitySource.transform.position, Quaternion.identity) as GameObject;
+		cameraPivot = planetWireframe.transform.Find("HUDPlanet_CameraPivot");
+		camera = cameraPivot.transform.Find("HUDPlanet_Camera");
+		
+		cameraPivot.LookAt(transform, transform.forward); // Aim the pivot at the player.
+		camera.localPosition = new Vector3(0.0f, 0.0f, cameraDistance); // Offset the camera to the appropriate distance to render the planet.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPivot.LookAt(transform, transform.forward); // Aim the pivot at the player.
+		camera.LookAt(cameraPivot, transform.forward); // Aim the pivot at the player.
+    }
+}
---
+++ GravityBlast/Assets/Scripts/SlowSpinTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowSpinTest : MonoBehaviour
+{
+    [SerializeField] Vector3 rotationVector;
+	
+    void Update()
+    {
+          transform.Rotate(rotationVector * Time.deltaTime, Space.Self);
+    }
+}
