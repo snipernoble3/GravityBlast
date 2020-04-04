b319e7bb50e125b9d2c041994a6fadc599530775
snipernoble3 <natelow@rocketmail.com>
2020-04-04 08:06:15 -0700

    Added Main Menu
    Currently only play and quit are hooked up, and play leads to the planet viewing scene rather than a playable one

---
+++ GravityBlast/Assets/Scripts/Game Management Scripts/Menu Scripts/CameraRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+    
+    private float tumbleSpeed;
+
+    void Start() {
+        tumbleSpeed = Random.Range(0.075f, 0.25f);
+        gameObject.GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumbleSpeed;
+    }
+}
---
+++ GravityBlast/Assets/Scripts/Game Management Scripts/Menu Scripts/MenuControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+    
+    public bool paused;
+
+    // MAIN MENU //
+
+    public void Play () {
+        SceneManager.LoadScene("Test_Planets");
+    }
+
+    public void Collection () {
+
+    }
+
+    public void Settings () {
+
+    }
+
+    public void Quit () {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+
+
+    // PAUSE MENU //
+
+    public void Pause () {
+        //open pause menu
+        //timescale 0
+    }
+
+    public void Resume () {
+        //close pause menu
+        //timescale 1
+    }
+
+    //Settings
+
+    public void Restart () {
+
+    }
+
+    public void ReturnToMenu () {
+
+    }
+
+    //Quit
+
+}
