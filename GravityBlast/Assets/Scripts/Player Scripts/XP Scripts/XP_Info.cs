2957ca0a5e2b40278d423547d5ef2a7fd47947f0
snipernoble3 <natelow@rocketmail.com>
2020-04-11 05:28:32 -0700

    Another merging commit, stuff was missed in the last one

---
+++ GravityBlast/Assets/Scripts/Player Scripts/XP Scripts/XP_Info.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XP_Info : MonoBehaviour {
+
+    [SerializeField] int xpToGive = 1;
+
+    private void OnTriggerEnter (Collider other) {
+        if (other.gameObject.GetComponent<Player_Stats>()) {
+            other.gameObject.GetComponent<Player_Stats>().CollectXP(xpToGive + (int)Random.value);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Update () {
+        //float above the ground
+    }
+
+}
