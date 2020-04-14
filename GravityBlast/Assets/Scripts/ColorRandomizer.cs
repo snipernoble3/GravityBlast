bb35c32f3ee7b72688e000efbb52eede114e6eeb
Xury Greer <Xury.Greer@Gmail.com>
2020-04-14 13:05:40 -0700

    Added color randomizer to planets.

---
+++ GravityBlast/Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour
+{
+    private Material planetSurface;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        planetSurface = GetComponent<Renderer>().material;
+		planetSurface.SetFloat("_Hue", Random.Range(0.0f, 1.0f));
+    }
+}
