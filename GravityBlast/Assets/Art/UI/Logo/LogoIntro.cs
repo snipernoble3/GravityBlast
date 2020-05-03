bf3fa501d2bfe37009704b3fa9fc5822a6208f12
Xury Greer <Xury.Greer@Gmail.com>
2020-05-03 13:55:26 -0700

    Replaced temporary main menu logo image with 3D real-time logo.

---
+++ GravityBlast/Assets/Art/UI/Logo/LogoIntro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoIntro : MonoBehaviour
+{
+    public Animator logo_Animator;
+	public GameObject logo_Solid;
+	public GameObject logo_Fractured;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForMusic());
+    }
+
+    IEnumerator WaitForMusic()
+    {
+        yield return new WaitForSeconds(6.8f);
+		FractureLogo();
+    }
+	
+	void FractureLogo()
+	{
+		logo_Solid.SetActive(false);
+		logo_Fractured.SetActive(true);
+		logo_Animator.SetFloat("playBackSpeed", 1.0f);
+	}
+}
