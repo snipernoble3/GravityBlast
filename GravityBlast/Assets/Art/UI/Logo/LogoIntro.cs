097ce3fb480bfb93233bbdf97ffb030aefeeee87
Xury Greer <Xury.Greer@Gmail.com>
2020-05-04 16:06:53 -0700

    Updated logo intro to use a better camera shake.

--- GravityBlast/Assets/Art/UI/Logo/LogoIntro.cs
+++ GravityBlast/Assets/Art/UI/Logo/LogoIntro.cs
@@ -4,40 +4,62 @@ using UnityEngine;
 
 public class LogoIntro : MonoBehaviour
 {
-    public Animator logo_CameraAnimator;
-	public Animator logo_Animator;
+    private Transform camPivot;
+	private Animator camPivot_Animator;
+	
+	private Transform logoCam;
+	private Vector3 logoCam_RestingPos;
+	
 	public GameObject logo_Solid;
 	public GameObject logo_Fractured;
+	private Animator fracture_Animator;
+	
+	private Transform gravityText;
+	
 	public CanvasGroup menuOptions;
-	public AudioSource music;
+	private AudioSource music;
 	
 	private Coroutine WaitForMusicCo;
+	private Coroutine ShakeCameraCo;
 	private Coroutine MenuFadeInCo;
 	
 	private bool introIsFinished = false;
 	
     void Start()
     {
-        music.Play();
+		camPivot = transform.Find("Camera Pivot");
+		gravityText = transform.Find("Gravity");
+		logoCam = camPivot.Find("Logo Camera");
+		
+		camPivot_Animator = camPivot.GetComponent<Animator>();
+		fracture_Animator = logo_Fractured.GetComponent<Animator>();
+		
+		logoCam_RestingPos = logoCam.position;
+		music = GetComponent<AudioSource>();
+        
+		music.Play();
 		
 		menuOptions.alpha = 0.0f;
 		menuOptions.interactable = false;
-		logo_CameraAnimator.Play("LogoCameraIntroAnimation", 0, 0.0f);
+		camPivot_Animator.Play("LogoCameraIntroAnimation", 0, 0.0f);
 		
 		WaitForMusicCo = StartCoroutine(WaitForMusic());
     }
 	
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Escape) && !introIsFinished)
+		if (!introIsFinished)
 		{
-			IntroEnd();
+			if (Input.GetKeyDown(KeyCode.Escape)) IntroEnd();
+			
+			gravityText.LookAt(new Vector3(gravityText.position.x, logoCam.position.y, logoCam.position.z));
 		}
 	}
 	
 	void IntroEnd()
 	{
 		if (WaitForMusicCo != null) StopCoroutine(WaitForMusicCo); // Stop the coroutine if it's still going.
+		if (ShakeCameraCo != null) StopCoroutine(ShakeCameraCo); // Stop the coroutine if it's still going.
 		if (MenuFadeInCo != null) StopCoroutine(MenuFadeInCo); // Stop the coroutine if it's still going.
 		
 		logo_Solid.SetActive(false);
@@ -45,8 +67,12 @@ public class LogoIntro : MonoBehaviour
 		
 		menuOptions.alpha = 1.0f; // Make the menu 100% visible.
 		menuOptions.interactable = true; // Make the menu interactable.
-		logo_CameraAnimator.Play("LogoCameraIntroAnimation", 0, 1.0f);  // Skip to the end of the camera animation;
-		logo_Animator.Play("Fracture", 0, 1.0f); // Skip to the end of the fracture animation;
+		
+		camPivot_Animator.Play("LogoCameraIntroAnimation", 0, 1.0f);  // Skip to the end of the camera animation;
+		
+		logoCam.position = logoCam_RestingPos;
+		
+		fracture_Animator.Play("Fracture", 0, 1.0f); // Skip to the end of the fracture animation;
 		
 		introIsFinished = true;
 	}
@@ -54,15 +80,41 @@ public class LogoIntro : MonoBehaviour
     IEnumerator WaitForMusic()
     {
         yield return new WaitForSeconds(7.0f);
-		FractureLogo();
+		
+		camPivot_Animator.Play("LogoCameraIntroAnimation", 0, 1.0f); // Skip to the end of the camera animation;
+		camPivot_Animator.enabled = false;
+		ShakeCameraCo = StartCoroutine(ShakeCamera());
     }
 	
 	void FractureLogo()
 	{
 		logo_Solid.SetActive(false);
 		logo_Fractured.SetActive(true);
-		logo_Animator.Play("Fracture", 0, 0.0f); // Play the fracture animation.
+		fracture_Animator.Play("Fracture", 0, 0.0f); // Play the fracture animation.
 		MenuFadeInCo = StartCoroutine(MenuFadeIn());
+	}
+	
+	IEnumerator ShakeCamera()
+	{	
+		float shakeProgress = 0.0f; // 0-1 value of progress through the shake.
+		float shakeDuration = 0.3f; // Duration of the shake in seconds.
+		float shakeIntensity = 0.07f; // The range of movement of the shake.
+		
+		while (shakeProgress < 1.0f)
+		{
+			shakeProgress = Mathf.Clamp(shakeProgress + Time.deltaTime / shakeDuration, 0.0f, 1.0f);
+			
+			float shakeRange = Mathf.Cos(shakeProgress * Mathf.PI * 2.0f) * shakeIntensity;
+
+			logoCam.position = logoCam_RestingPos; // Reset the position to avoid compounding effect from the following statement.
+			logoCam.position = logoCam.TransformPoint(new Vector3(Random.Range(-shakeRange, shakeRange), Random.Range(-shakeRange, shakeRange), 0.0f));
+			
+			yield return null;
+		}
+		
+		logoCam.position = logoCam_RestingPos;
+		
+		FractureLogo();
 	}
 		
 	IEnumerator MenuFadeIn()
