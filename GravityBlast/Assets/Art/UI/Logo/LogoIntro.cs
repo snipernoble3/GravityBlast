0da4d7ceef15063530c2aed1edb906f84df3f290
Xury Greer <Xury.Greer@Gmail.com>
2020-05-03 17:02:49 -0700

    Improved intro animation for logo and added the ability to skip it with the ESC key.

--- GravityBlast/Assets/Art/UI/Logo/LogoIntro.cs
+++ GravityBlast/Assets/Art/UI/Logo/LogoIntro.cs
@@ -8,17 +8,48 @@ public class LogoIntro : MonoBehaviour
 	public Animator logo_Animator;
 	public GameObject logo_Solid;
 	public GameObject logo_Fractured;
+	public CanvasGroup menuOptions;
+	public AudioSource music;
 	
-	// Start is called before the first frame update
+	private Coroutine WaitForMusicCo;
+	private Coroutine MenuFadeInCo;
+	
     void Start()
     {
-        logo_CameraAnimator.Play("LogoCameraIntroAnimation", 0, 0.0f);
-		StartCoroutine(WaitForMusic());
+        music.Play();
+		
+		menuOptions.alpha = 0.0f;
+		menuOptions.interactable = false;
+		logo_CameraAnimator.Play("LogoCameraIntroAnimation", 0, 0.0f);
+		
+		WaitForMusicCo = StartCoroutine(WaitForMusic());
     }
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			IntroEnd();
+		}
+	}
+	
+	void IntroEnd()
+	{
+		if (WaitForMusicCo != null) StopCoroutine(WaitForMusicCo); // Stop the coroutine if it's still going.
+		if (MenuFadeInCo != null) StopCoroutine(MenuFadeInCo); // Stop the coroutine if it's still going.
+		
+		logo_Solid.SetActive(false);
+		logo_Fractured.SetActive(true);
+		
+		menuOptions.alpha = 1.0f; // Make the menu 100% visible.
+		menuOptions.interactable = true; // Make the menu interactable.
+		logo_CameraAnimator.Play("LogoCameraIntroAnimation", 0, 1.0f);  // Skip to the end of the camera animation;
+		logo_Animator.Play("Fracture", 0, 1.0f); // Skip to the end of the fracture animation;
+	}
 
     IEnumerator WaitForMusic()
     {
-        yield return new WaitForSeconds(6.7f);
+        yield return new WaitForSeconds(7.0f);
 		FractureLogo();
     }
 	
@@ -26,6 +57,23 @@ public class LogoIntro : MonoBehaviour
 	{
 		logo_Solid.SetActive(false);
 		logo_Fractured.SetActive(true);
-		logo_Animator.SetFloat("playBackSpeed", 0.25f);
+		logo_Animator.Play("Fracture", 0, 0.0f); // Play the fracture animation.
+		MenuFadeInCo = StartCoroutine(MenuFadeIn());
+	}
+		
+	IEnumerator MenuFadeIn()
+	{	
+		float fadeValue = 0.0f;
+		float fadeDuration = 1.0f; // How long it takes for the text to fade in in seconds.
+		
+		while (fadeValue < 1.0f)
+		{
+			fadeValue = Mathf.Clamp(fadeValue + Time.deltaTime / fadeDuration, 0.0f, 1.0f);
+			menuOptions.alpha = fadeValue;
+			
+			yield return null;
+		}
+		
+		IntroEnd();
 	}
 }
