30d45aa01c5c9c4dcbd0b736e8f4164fceea1098
Xury Greer <Xury.Greer@Gmail.com>
2020-05-13 18:01:14 -0700

    Added Sean's crystal meshes to XP cubes.

---
+++ GravityBlast/Assets/Scripts/MeshRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRandomizer : MonoBehaviour
+{
+    [SerializeField] Mesh[] meshes;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+		int rando = Random.Range(0, meshes.Length);
+		
+		GetComponent<MeshFilter>().mesh = meshes[rando];
+    }
+}
