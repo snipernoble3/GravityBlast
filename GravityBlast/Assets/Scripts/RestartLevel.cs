7e92b8d186a80eb1e481bddc71a1a783f958b3a6
Xury Greer <Xury.Greer@Gmail.com>
2020-06-16 02:31:15 -0700

    Finished first two shots.

--- GravityBlast/Assets/Scripts/RestartLevel.cs
+++ GravityBlast/Assets/Scripts/RestartLevel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RestartLevel {
+public class RestartLevel : MonoBehaviour
+{
     Scene scene;
 	int numOfScenes;
 	int levelToLoad;
+	
+	private void Update ()
+	{
+		//if (Input.GetButtonDown("Restart")) Restart(); //"Restart" is currently bound to J
+		if (Input.GetButtonDown("Restart")) SceneManager.LoadScene(0);
+	}
 	
 	public RestartLevel () {
 		numOfScenes = SceneManager.sceneCountInBuildSettings - 1; // Offset so that we can ask for the scene by index more easily.
