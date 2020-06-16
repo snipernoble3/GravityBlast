45afa22470ab0c2e58b3202e95c37100712901d6
Xury Greer <Xury.Greer@Gmail.com>
2020-06-16 04:45:41 -0700

    Added DoF control

---
+++ GravityBlast/Assets/Scenes/CutScene/DOFHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[ExecuteAlways]
+public class DOFHelper : MonoBehaviour
+{
+    [SerializeField] private VolumeProfile postProcessing;
+	
+	public float focusDistance;
+	private float previousFocusDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (focusDistance != previousFocusDistance)
+		{
+			DepthOfField dof;
+            if (postProcessing.TryGet(out dof))
+            {
+				dof.focusDistance.value = focusDistance;
+				previousFocusDistance = focusDistance;
+			}
+		}
+    }
+}
